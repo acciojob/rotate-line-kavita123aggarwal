3eeae26c388d07d66bbb758e2d5a9c9ab9be7d7e
kavita123aggarwal <118376629+kavita123aggarwal@users.noreply.github.com>
2023-09-29 11:11:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+ const line = document.getElementById('line');
+        let rotationAngle = 0;
+
+        function rotateLine() {
+            rotationAngle += 2;
+            line.style.transform = `translate(-50%, -50%) rotate(${rotationAngle}deg)`;
+            requestAnimationFrame(rotateLine);
+        }
+
+        rotateLine();
